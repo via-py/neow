f9c9b42d5ab7bce2e3223a7decf6e8f14d110f0a
ZMZhang <zhangzheming_zzm@foxmail.com>
2019-08-19 16:22:46 +0800

    Add routing rules

--- api/ProxyApi.py
+++ api/ProxyApi.py
@@ -30,10 +30,66 @@ class JsonResponse(Response):
 
 app.response_class = JsonResponse
 
+api_list = {
+    'get': '',
+    'get_all': '',
+    'delete?num=': '',
+    'get_status': ''
+}
+
 
 @app.route('/')
-def hello_world():
-    return 'Start-up Test!!!'
+def index():
+    """
+    请求列表
+    :return:
+    """
+    return api_list
+
+
+@app.route('/get/')
+def get():
+    """
+    获取单条IP记录请求
+    :return:
+    """
+    return None
+
+
+@app.route('/get_all/')
+def getAll():
+    """
+    获取多条IP记录请求
+    :return:
+    """
+    return None
+
+
+@app.route('/refresh/')
+def refresh():
+    """
+    刷新IP记录请求
+    :return:
+    """
+    return None
+
+
+@app.route('/get_status/')
+def getStatus():
+    """
+    获取IP存活状态
+    :return:
+    """
+    return None
+
+
+@app.route('/delete/', methods=['GET'])
+def delete():
+    """
+    请求删除IP数据记录
+    :return:
+    """
+    return None
 
 
 if __name__ == '__main__':
